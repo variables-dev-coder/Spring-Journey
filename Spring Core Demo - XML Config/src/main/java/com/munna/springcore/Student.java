79a886f74c59f17253d9a5d989ff5b1abdba74ca
Munna Aziz Mondal <azizmunna661@gmail.com>
2025-07-29 22:11:37 +0530

    Initial upload: multiple Spring demo projects

---
+++ Spring Core Demo - XML Config/src/main/java/com/munna/springcore/Student.java
@@ -0,0 +1,21 @@
+package com.munna.springcore;
+
+public class Student {
+	
+	    private int id;
+	    private String name;
+
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public void display() {
+	        System.out.println("Student ID: " + id);
+	        System.out.println("Student Name: " + name);
+	    }
+	}
+
